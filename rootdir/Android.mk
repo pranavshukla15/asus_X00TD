ecf21c52b1f6fce03f5346c663125f413c3e31b7
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-11-12 19:29:06 -0200

    X00T: Introduce 'SafailNet'
    Bypass safetynet even if we have permissive selinux
    
    Change-Id: Ib44cdbd15b53af3a1768cb0912f2dbcc56a8d97e
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -158,5 +158,12 @@ LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
 LOCAL_SRC_FILES       := sbin/chargeonlymode
 LOCAL_MODULE_TAGS     := optional
 LOCAL_MODULE_CLASS    := EXECUTABLES
+
+include $(BUILD_PREBUILT)
+LOCAL_MODULE       := init.safailnet.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.safailnet.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
